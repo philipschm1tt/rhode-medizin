523bc676a6fc085397de4d80aca89119175dd224
Philip Schmitt <philip.schmitt@outlook.com>
2018-08-07 11:55:58 +0200

    Setup styled-components and stylelint. Use configuration from gatsby-starter-modern.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 
+const Headline = styled.h1`
+  color: blue;
+`
+
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <Headline>Hi people</Headline>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
   </Layout>
