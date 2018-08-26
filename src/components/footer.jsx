97e47fad8be841b18c4dcb006d4a6e648120b66a
Philip Schmitt <philip.schmitt@outlook.com>
2018-08-26 11:10:27 +0200

    Add contentful link to footer as per https://www.contentful.com/r/knowledgebase/how-attribution-works/ This may no longer be necessary with the new contentful plans, but I am not sure.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,8 +15,12 @@ const FooterArea = MainGrid.withComponent('footer').extend`
 
 const FooterLink = styled(Link)`
   color: ${props => props.theme.colors.lightYellow};
-  margin-left: ${props => props.theme.sizes.baseLineHeight};
+  margin-right: ${props => props.theme.sizes.baseLineHeight};
+`
+
+const ExternalFooterLink = styled.a`
   float: right;
+  width: 100px;
 `
 
 const Footer = () => (
@@ -24,6 +28,16 @@ const Footer = () => (
     <ContentBox extraVerticalPadding="true">
       <FooterLink to="/imprint/">Impressum</FooterLink>
       <FooterLink to="/dataPolicy/">Datenschutzhinweis</FooterLink>
+      <ExternalFooterLink
+        href="https://www.contentful.com/"
+        rel="nofollow noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          src="https://images.ctfassets.net/fo9twyrwpveg/7Htleo27dKYua8gio8UEUy/0797152a2d2f8e41db49ecbf1ccffdaa/PoweredByContentful_DarkBackground_MonochromeLogo.svg"
+          alt="Powered by Contentful"
+        />
+      </ExternalFooterLink>
     </ContentBox>
   </FooterArea>
 )
