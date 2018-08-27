2eec902511b1a3fbc1f017cff6f1f5d21bb6d700
Philip Schmitt <philip.schmitt@outlook.com>
2018-08-27 19:42:09 +0200

    Remove vertical space from product groups for phones – and all other sizes.

--- src/components/tileList.jsx
+++ src/components/tileList.jsx
@@ -4,11 +4,6 @@ const TileList = styled.div`
   > * {
     margin-bottom: ${props => props.theme.sizes.baseLineHeight};
   }
-
-  @supports (display: grid) {
-    display: grid;
-    grid-auto-rows: 1fr;
-  }
 `
 
 export default TileList
